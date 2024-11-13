9c29e4b3de1c40cd2e45c06d81caf1c6c6120a37
AnuragMaurya42 <am9648676889@gmail.com>
2024-11-14 00:40:28 +0530

    first

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const db = require("./models");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
@@ -17,9 +17,17 @@ app.use("/api/faculty", faculty);
 app.use("/api/student", student);
 
 const PORT = process.env.PORT || 5000;
+const mongoURI = "mongodb://127.0.0.1:27017/anurag"; // or "mongodb://localhost:27017/anurag"
 
-db.sequelize.sync().then(() => {
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
   app.listen(PORT, () => {
-    console.log(`Listening on: http://localhost:${PORT}`);
+    console.log(`Server running on: http://localhost:${PORT}`);
   });
+})
+.catch((err) => {
+  console.error("Database connection error:", err);
 });
