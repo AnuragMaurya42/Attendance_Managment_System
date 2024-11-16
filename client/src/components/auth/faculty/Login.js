370a55636d21893833131f6f35e48cb2380ad868
Harsh bindal <108680668+Harsh-bindal@users.noreply.github.com>
2024-11-17 01:01:40 +0530

    logIn pages modified

--- client/src/components/auth/faculty/Login.js
+++ client/src/components/auth/faculty/Login.js
@@ -22,6 +22,8 @@ const FacultyLogin = ({
 
   const { name, email, password, dept } = formData;
 
+  const [loginTag, setLoginTag] = useState(true);
+
   let signUpButton = document.getElementById("signUp");
   let signInButton = document.getElementById("signIn");
   let container = document.getElementById("container");
@@ -61,83 +63,111 @@ const FacultyLogin = ({
 
   return (
     <div className="body">
-      <h1 style={{ textAlign: "center", fontWeight: "700", fontSize: "50" }}>
+      <h1 style={{ textAlign: "center", fontWeight: "700", fontSize: "50px" }}>
         <b>Faculty Login</b>
       </h1>
+
       <div className="container" id="container">
-        <div className="form-container sign-up-container">
-          <form action="#" onSubmit={(e) => register(e)}>
-            <h1>Create Account</h1>
-            <input
-              type="text"
-              placeholder="Name"
-              value={name}
-              name="name"
-              onChange={(e) => onChange(e)}
-              autoComplete="off"
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              name="email"
-              onChange={(e) => onChange(e)}
-              autoComplete="off"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              name="password"
-              onChange={(e) => onChange(e)}
-              autoComplete="off"
-            />
-            <input
-              type="text"
-              placeholder="Department"
-              value={dept}
-              name="dept"
-              onChange={(e) => onChange(e)}
-              autoComplete="off"
-            />
-            <button type="submit">Sign Up</button>
-          </form>
-        </div>
-        <div className="form-container sign-in-container">
-          <form action="#" onSubmit={(e) => login(e)}>
-            <h1>Sign in</h1>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              name="email"
-              onChange={(e) => onChange(e)}
-              autoComplete="off"
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              name="password"
-              onChange={(e) => onChange(e)}
-              autoComplete="off"
-            />
-            <button>Sign In</button>
-          </form>
-        </div>
+        {loginTag === false ? (
+          <div className="form-container sign-up-container">
+            <form action="#" onSubmit={(e) => register(e)}>
+              <h1>Create Account</h1>
+              <input
+                type="text"
+                placeholder="Name"
+                value={name}
+                name="name"
+                onChange={(e) => onChange(e)}
+                autoComplete="off"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                name="email"
+                onChange={(e) => onChange(e)}
+                autoComplete="off"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                name="password"
+                onChange={(e) => onChange(e)}
+                autoComplete="off"
+              />
+              <input
+                type="text"
+                placeholder="Department"
+                value={dept}
+                name="dept"
+                onChange={(e) => onChange(e)}
+                autoComplete="off"
+              />
+              <button type="submit">Sign Up</button>
+            </form>
+            <button
+              onClick={() => toggleForm("signIn")}
+              className="newButton"
+              id="signIn"
+            >
+              Already have an account!
+            </button>
+          </div>
+        ) : (
+          <div className="form-container sign-in-container">
+            <form action="#" onSubmit={(e) => login(e)}>
+              <h1>Sign In</h1>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                name="email"
+                onChange={(e) => onChange(e)}
+                autoComplete="off"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                name="password"
+                onChange={(e) => onChange(e)}
+                autoComplete="off"
+              />
+              <button type="submit">Sign In</button>
+            </form>
+            <button
+              onClick={() => toggleForm("signUp")}
+              className="newButton"
+              id="signUp"
+            >
+              Don't have an account?
+            </button>
+          </div>
+        )}
+
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
               <h1>Already have an account!</h1>
               <p>Please login to access your profile</p>
-              <button onClick={removeClass} className="ghost" id="signIn">
+              <button
+                onClick={() => toggleForm("signIn")}
+                className="ghost"
+                id="signIn"
+              >
                 Sign In
               </button>
             </div>
+
             <div className="overlay-panel overlay-right">
               <h1>Don't have an account?</h1>
               <p>Go ahead and create an account for yourself!!</p>
-              <button onClick={addClass} className="ghost" id="signUp">
+              <button
+                onClick={() => toggleForm("signUp")}
+                className="ghost"
+                id="signUp"
+              >
                 Sign Up
               </button>
             </div>
@@ -146,6 +176,18 @@ const FacultyLogin = ({
       </div>
     </div>
   );
+
+  // JavaScript for toggling forms
+  function toggleForm(targetForm) {
+    const container = document.getElementById("container");
+    if (targetForm === "signUp") {
+      container.classList.add("right-panel-active");
+      setLoginTag(false);
+    } else {
+      container.classList.remove("right-panel-active");
+      setLoginTag(true);
+    }
+  }
 };
 
 FacultyLogin.propTypes = {
